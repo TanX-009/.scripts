cfcf6f60a86e075fc5ecf895de90d11e93392125
TanX-009 <tanmaymuley009@gmail.com>
2024-06-04 23:04:59 +0530

    updated to use Adw-dark

--- reload/apply-gradience.bash
+++ reload/apply-gradience.bash
@@ -3,5 +3,5 @@
 mkdir -p "$HOME/.config/presets" # create gradience presets folder
 gradience-cli apply -p "$HOME/.colors/gtk/preset.json" --gtk both
 
-gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
+gsettings set org.gnome.desktop.interface gtk-theme 'Adw-dark'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
